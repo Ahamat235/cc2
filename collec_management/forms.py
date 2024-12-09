591d2767136483b10108d58276178a5c5e5ce594
Ahamat235 <ahamatmoussaabdelkerim@gmail.com>
2024-12-09 10:50:34 +0100

    Add files via upload

--- collec_management/forms.py
+++ collec_management/forms.py
@@ -1,5 +1,5 @@
 from django . forms import ModelForm
-from collec_management.models import Collec
+from collec_management.models import Collec, Element
 
 
 class CollecForm(ModelForm) :
@@ -8,4 +8,15 @@ class CollecForm(ModelForm) :
           fields=["title" , "description" ]
           labels={"title" : "Titre" ,
                     "description" : "Description" 
+                    }
+          
+class ElementForm(ModelForm) :
+     class Meta :
+          model= Element
+          fields=["title" , "description","value","quantity","collection"]
+          labels={"title" : "Titre" ,
+                    "description" : "Description" ,
+                    "quantity":"Quantite",
+                    "value" : "Valeur",
+                    "collection":"ID collection"
                     }
--- collec_management/views.py
+++ collec_management/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
-from collec_management.forms import CollecForm
-from collec_management.models import Collec
+from collec_management.forms import CollecForm, ElementForm
+from collec_management.models import Collec, Element
 # Create your views here.
 def about(request):
     return render(request, 'collec_management/about.html')
@@ -56,3 +56,39 @@ def change(request , collec_id) :
                  ,{"form": form})
 def menu(request):
     return render(request, 'collec_management/menu.html')
+ 
+# les vues des elements 
+def delete_element(request , element_id) :
+   element=get_object_or_404(Element , pk=element_id)
+   collection=element.collection.id
+   if request.method=="POST" :
+      element.delete()
+      return HttpResponseRedirect(reverse('collection' ,args=[collection] ))
+   return render (request , "collec_management/element_delete.html",{"element" :element})
+
+def add( request ) :
+ if request.method == "POST" :
+    form=ElementForm(request.POST)
+    if form.is_valid () :
+       element=form.save(commit = False ) 
+       element.save () # Sauvegarde en base de donnees
+       return HttpResponseRedirect(reverse('element' ,args=[element.id] ))
+ else :
+    form = ElementForm() # Formulaire vide
+ return render(request,"collec_management/formulaireAdd.html" ,
+               {"form" :form })
+def element(request ,id) :
+    elm = get_object_or_404(Element , id=id)
+    return render(request ,'collec_management/element_detail.html', {'elm' : elm})
+
+def element_edit(request, id):
+    elm = get_object_or_404(Element , pk=id)
+    if request.method == "POST" :
+       form =ElementForm(request.POST , instance=elm)
+       elmModif_id = check_save(form)
+       return redirect("element" , id = elmModif_id)
+    else :
+       form =ElementForm(instance=elm)
+    return render(request,
+                  "collec_management/element_modifier.html"
+                  ,{"form": form})
